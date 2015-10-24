5eb71a05f49f7d65040c7e85077035d8d154b2b3
Kyle Morehead <krmorehead@gmail.com>
2015-10-23 21:38:05 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
+function addQuotes(variable){return "\"" + variable.toString() + "\""}
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+	if(typeof(obj) === "function" || obj === undefined){return undefined}
+	else if(typeof(obj) === "string"){return  addQuotes(obj) }
+	else if(typeof(obj)!== "object"){return obj.toString()}
+	else if(obj === null){return 'null'}
+
+	var array = Array.isArray(obj);
+	var repeated = false;
+	var result = ""; 
+
+	if(array){result += "["}
+	else{result += '{'}
+	// only array and objects left
+	_.each(obj, function(el, key){
+
+		//recursive call
+		var recursiveEl = stringifyJSON(el)
+
+		//don't want commas or : for unstringfiable items
+		if(recursiveEl){
+
+			// start adding commas after the first object
+			if(repeated){result += ","}
+
+			if(!array){result += addQuotes(key) +":"}
+
+			result += stringifyJSON(el);
+			repeated = true;		
+		}
+	})
+
+	if(array){result += "]"}
+	else{result += '}'}
+
+	return result
 };
