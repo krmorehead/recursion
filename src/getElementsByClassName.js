dcad66af17ff895d92dcc03210ba4609533cbac7
Kyle Morehead <krmorehead@gmail.com>
2015-10-24 01:42:58 -0700

    Finished ElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, element){
+	if(element === undefined){element = document.body}
+
+	var result = []
+	if(_.include(element.classList, className)){
+		result.push(element)
+	}
+
+	var children = element.childNodes
+	if(children.length){
+		_.each(children, function(child){
+			//Recursive call, hits children and subsequent children
+			result.push(...getElementsByClassName(className, child))
+		})
+	}
+
+	//BASECASE
+	else{return result}
+
+	//After finishing the children
+	return result
 };
+
+
+/*
+document.body, element.childNodes, and element.classList
+*/
